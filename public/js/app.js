bed0763cb52d3fd1690046f6a7416dc46069289b
Joska Potin <jpotin@gmail.com>
2021-08-08 20:34:09 +0200

    construction ok photographer and medias linked

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,92 @@
 const url = "./data/fisheyedata.json";
 const mediasPath = "medias/";
 const photographeThumbPath = mediasPath + "photographers-id-photos/";
+const photographersTable = document.querySelector("#photographes-table__body");
+const mediasTable = document.querySelector("#medias-table__body");
 
-const tablePhotographers = document.querySelector("#photographes-table__body");
-const tableMedia = document.querySelector("#medias-table__body");
+// photographers factory
+const photographersFactory = (id, firstname, lastname, city, country, tags, tagline, price, portrait) => {
+  const medias = [];
+  const addMedia = element => {
+    medias.push(element);
+  };
+  return { id, firstname, lastname, city, country, tags, tagline, price, portrait, medias, addMedia };
+};
 
-const addDatas = (photographers, medias) => {
-  photographers.forEach(photographer => {
-    const { name, id, city, country, tags, tagline, price, portrait } = photographer;
+// medias factory
+const mediasFactory = (id, photographerId, title, image, video, tags, likes, date, price) => {
+  const photographer = [];
+  const addPhotographer = element => {
+    photographer.push(element);
+  };
+  return { id, photographerId, title, image, video, tags, likes, date, price, photographer, addPhotographer };
+};
+
+// create photographers
+const createPhotographers = elements => {
+  const photographers = [];
+  elements.forEach(element => {
+    const { id, name, city, country, tags, tagline, price, portrait } = element;
     const fullname = name.split(" ");
     const firstname = fullname[0];
     const lastname = fullname[1];
-    const tagString = tags.join(", ");
-    const thumbPath = photographeThumbPath.concat(portrait);
-    const thumb = `<img src="${thumbPath}" alt="Portrait de ${name}" class="thumbnail"/>`;
-
-    const photographerMedias = medias.filter(item => item.photographerId === id);
-    const titles = [];
-    photographerMedias.forEach(item => {
-      const title = item.title;
-      titles.push(title);
-    });
-    const photographerMediasTitles = titles.join(", ");
-
-    const imagesArray = [];
-    photographerMedias.forEach(item => {
-      const image = item.image;
-      imagesArray.push(image);
-    });
-
-    const createImagesPreview = array => {
-      const imagesPreview = [];
-      array.forEach(image => {
-        const path = mediasPath.concat("", firstname).toLowerCase().concat("/", image);
-        const imageSrc = `<img class="thumb-preview" src="${path}"/>`;
-        imagesPreview.push(imageSrc);
-      });
-      return imagesPreview.join("");
-    };
-    console.log(createImagesPreview(imagesArray));
-    const photographerImagesPreview = createImagesPreview(imagesArray);
-
-    const row = `<tr><td>${name}</td><td>${id}</td><td>${city}</td><td>${country}</td><td>${tagString}</td><td>${tagline}</td><td>${price}</td><td>${thumb}</td><td>${photographerImagesPreview}</td>
-    </tr>`;
-    tablePhotographers.insertAdjacentHTML("beforeend", row);
+    const photographer = photographersFactory(id, firstname, lastname, city, country, tags, tagline, price, portrait);
+    photographers.push(photographer);
   });
+  return photographers;
+};
 
-  medias.forEach(element => {
-    const { id, photographerId, title, image, tags, likes, date, price } = element;
-    const tagString = tags.join(", ");
-
-    const mediasPhotographer = photographers.filter(item => item.id === photographerId);
-    const name = mediasPhotographer[0].name;
-
-    const row = `<tr><td>${id}</td><td>${photographerId}</td><td>${name}</td><td>${title}</td><td>${image}</td><td>${tagString}</td><td>${likes}</td><td>${date}</td><td>${price}</td></tr>`;
-    tableMedia.insertAdjacentHTML("beforeend", row);
+// create medias
+const createMedias = elements => {
+  const mediasInit = [];
+  elements.forEach(element => {
+    const { id, photographerId, title, image, video, tags, likes, date, price } = element;
+    const media = mediasFactory(id, photographerId, title, image, video, tags, likes, date, price);
+    mediasInit.push(media);
   });
+  return mediasInit;
+};
+
+// get media by photographer
+const getMediasByPhotographer = (array, id) => {
+  const photographerMedias = array.filter(element => element.photographerId === id);
+  return photographerMedias;
+};
+
+// get photographer by id
+const getPhotographerById = (array, id) => {
+  return array.find(element => element.id === id);
+};
+
+// add medias to photographers
+const addMediasToPhotographer = (photographersArray, mediasArray) => {
+  photographersArray.forEach(photographer => {
+    const media = getMediasByPhotographer(mediasArray, photographer.id);
+    media.forEach(element => {
+      photographer.addMedia(element);
+    });
+  });
+  return photographersArray;
+};
+
+// add photographer to media
+const addPhotographerToMedia = (photographersArray, mediasArray) => {
+  mediasArray.forEach(media => {
+    const photographer = getPhotographerById(photographersArray, media.photographerId);
+    media.addPhotographer(photographer.firstname);
+    media.addPhotographer(photographer.lastname);
+  });
+  return mediasArray;
+};
+
+// Handle construction
+const construction = ({ photographers, media }) => {
+  const photographersInit = createPhotographers(photographers);
+  const mediasInit = createMedias(media);
+  const photographersArray = addMediasToPhotographer(photographersInit, mediasInit);
+  const mediasArray = addPhotographerToMedia(photographersInit, mediasInit);
+  addPhotographersTable(photographersArray);
+  addMediasTable(mediasArray);
 };
 
 // Fetch function
@@ -70,7 +101,31 @@ const getData = async url => {
 // Get Datas from fetch function
 getData(url)
   .then(data => {
-    const { photographers, media } = data;
-    addDatas(photographers, media);
+    construction(data);
   })
   .catch(error => console.log(error));
+
+// add photographers to the table
+const addPhotographersTable = elements => {
+  elements.forEach(element => {
+    const { id, firstname, lastname, city, country, tags, tagline, price, portrait, medias } = element;
+    const tagString = tags.join(", ");
+    const titlesArray = [];
+    medias.forEach(element => {
+      titlesArray.push(element.title);
+    });
+    const titles = titlesArray.join(", ");
+    const row = `<tr><td>${id}</td><td>${firstname}</td><td>${lastname}</td><td>${city}</td><td>${country}</td><td>${tagString}</td><td>${tagline}</td><td>${price}</td><td>${portrait}</td><td>${titles}</td></tr>`;
+    photographersTable.insertAdjacentHTML("beforeend", row);
+  });
+};
+
+// add medias to the table
+const addMediasTable = elements => {
+  elements.forEach(element => {
+    const { id, photographerId, title, image, video, tags, likes, date, price, photographer } = element;
+    const tagString = tags.join(", ");
+    const row = `<tr><td>${id}</td><td>${photographerId}</td><td>${photographer[0]} ${photographer[1]}</td><td>${title}</td><td>${image}</td><td>${video}</td><td>${tagString}</td><td>${likes}</td><td>${date}</td><td>${price}</td></tr>`;
+    mediasTable.insertAdjacentHTML("beforeend", row);
+  });
+};
