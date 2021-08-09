a0e5f59ac638dcdfc7d57f9125591b15d02f9a3a
joskapotin <joskapotin@users.noreply.github.com>
2021-08-09 20:35:41 +0000

    tag nav, modules and stuff 422c285e6568f8cfaae52ddd58e7653515e885d6

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,101 +1,66 @@
+import buildData from "./builder.js";
+import { getAllTheTag, createTaglist, getPhotographersByTag } from "./helpers.js";
+
 const url = "./data/fisheyedata.json";
 const mediasPath = "medias/";
 const photographeThumbPath = mediasPath + "photographers-id-photos/";
 const uiPhotographersList = document.querySelector("#photographers-list");
 const uiTagNav = document.querySelector("#tag-nav");
 
-const photographersTable = document.querySelector("#photographes-table__body"); // test only
-const mediasTable = document.querySelector("#medias-table__body"); // test only
-
-// photographers factory
-const photographersFactory = (id, firstname, lastname, city, country, tags, tagline, price, portrait) => {
-  const medias = [];
-  const addMedia = element => {
-    medias.push(element);
-  };
-  return { id, firstname, lastname, city, country, tags, tagline, price, portrait, medias, addMedia };
+// Clear photographers list
+const uiClearPhotographersList = () => {
+  const uiPhotographers = document.querySelectorAll(".card");
+  return uiPhotographers.forEach(element => element.remove());
 };
 
-// medias factory
-const mediasFactory = (id, photographerId, title, image, video, tags, likes, date, price) => {
-  const photographer = [];
-  const addPhotographer = element => {
-    photographer.push(element);
-  };
-  return { id, photographerId, title, image, video, tags, likes, date, price, photographer, addPhotographer };
+// showPhotographersbyTag
+const uiShowPhotographersbyTag = (photographers, tag) => {
+  const photographersArray = getPhotographersByTag(photographers, tag);
+  uiClearPhotographersList();
+  uiCreatePhotographersList(photographersArray);
 };
 
-// create photographers
-const createPhotographers = elements => {
-  const photographers = [];
-  elements.forEach(element => {
-    const { id, name, city, country, tags, tagline, price, portrait } = element;
-    const fullname = name.split(" ");
-    const firstname = fullname[0];
-    const lastname = fullname[1];
-    const photographer = photographersFactory(id, firstname, lastname, city, country, tags, tagline, price, portrait);
-    photographers.push(photographer);
+// Add event listeners
+const initTagNav = photographers => {
+  const uiNavTags = uiTagNav.querySelectorAll(".tag-link");
+  uiNavTags.forEach(element => {
+    element.addEventListener("click", function (event) {
+      // event.preventDefault();
+      const tag = event.target.getAttribute("href").substring(1);
+      uiShowPhotographersbyTag(photographers, tag);
+    });
   });
+};
+
+// Create tag nav
+const uiCreateTagList = photographers => {
+  const tagArray = getAllTheTag(photographers);
+  const tagList = createTaglist(tagArray);
+
+  uiTagNav.insertAdjacentHTML("beforeend", tagList);
+};
+
+// Create photographers list
+const uiCreatePhotographersList = photographers => {
+  photographers.forEach(photographe => {
+    const { firstname, lastname, city, country, tags, tagline, price, portrait } = photographe;
+    const thumbnail = photographeThumbPath.concat(portrait);
+    const tagList = tags.join('</li><li class="tag">');
+
+    const uiCard = `<article class="card"><img class="card__img" src="${thumbnail}" height="200" width="200"><h2 class="name">${firstname} ${lastname}</h2><h3 class="location">${city}, ${country}</h3><p class="tagline">${tagline}</p><p class="pricing">${price}€/jour</p><ul class="tag-list"><li class="tag">${tagList}</li></ul></article>`;
+    uiPhotographersList.insertAdjacentHTML("beforeend", uiCard);
+  });
+};
+
+// Init HTML
+const initHtml = ({ photographers, medias }) => {
+  uiCreatePhotographersList(photographers);
+  uiCreateTagList(photographers);
   return photographers;
 };
 
-// create medias
-const createMedias = elements => {
-  const mediasInit = [];
-  elements.forEach(element => {
-    const { id, photographerId, title, image, video, tags, likes, date, price } = element;
-    const media = mediasFactory(id, photographerId, title, image, video, tags, likes, date, price);
-    mediasInit.push(media);
-  });
-  return mediasInit;
-};
-
-// get media by photographer
-const getMediasByPhotographer = (array, id) => {
-  const photographerMedias = array.filter(element => element.photographerId === id);
-  return photographerMedias;
-};
-
-// get photographer by id
-const getPhotographerById = (array, id) => {
-  return array.find(element => element.id === id);
-};
-
-// add medias to photographers
-const addMediasToPhotographer = (photographersArray, mediasArray) => {
-  photographersArray.forEach(photographer => {
-    const media = getMediasByPhotographer(mediasArray, photographer.id);
-    media.forEach(element => {
-      photographer.addMedia(element);
-    });
-  });
-  return photographersArray;
-};
-
-// add photographer to media
-const addPhotographerToMedia = (photographersArray, mediasArray) => {
-  mediasArray.forEach(media => {
-    const photographer = getPhotographerById(photographersArray, media.photographerId);
-    media.addPhotographer(photographer.firstname); // test only
-    media.addPhotographer(photographer.lastname); // test only
-  });
-  return mediasArray;
-};
-
-// Handle construction
-const construction = ({ photographers, media }) => {
-  const photographersInit = createPhotographers(photographers);
-  const mediasInit = createMedias(media);
-  const photographersArray = addMediasToPhotographer(photographersInit, mediasInit);
-  const mediasArray = addPhotographerToMedia(photographersInit, mediasInit);
-  uiCreatePhotographersList(photographersArray);
-  uiCreateTagList(photographersArray);
-};
-
-// Populate HTML
-
 // Fetch function
-const getData = async url => {
+const fetchData = async url => {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Erreur HTTP ! statut : ${response.status}`);
@@ -103,70 +68,15 @@ const getData = async url => {
   return response.json();
 };
 
-// Get Datas from fetch function
-getData(url)
-  .then(data => {
-    construction(data);
+// Get Datas from fetch then run build objects then run build html
+fetchData(url)
+  .then(result => {
+    return buildData(result);
+  })
+  .then(result => {
+    return initHtml(result);
+  })
+  .then(result => {
+    return initTagNav(result);
   })
   .catch(error => console.log(error));
-
-// get all the tags
-const getAllTheTag = elements => {
-  const tagArray = [];
-  elements.forEach(element => {
-    element.tags.forEach(item => {
-      if (!tagArray.includes(item)) {
-        tagArray.push(item);
-      }
-    });
-  });
-  return tagArray;
-};
-
-// create tags list
-const uiCreateTagList = elements => {
-  const tagArray = getAllTheTag(elements);
-  const tagList = tagArray.join('</button></li><li class="tag"><button class="btn-tag">#');
-
-  const uiTagList = `<li class="tag"><button class="btn-tag">#${tagList}</button></li>`;
-  uiTagNav.insertAdjacentHTML("beforeend", uiTagList);
-};
-
-// create photographers list
-const uiCreatePhotographersList = elements => {
-  elements.forEach(element => {
-    const { firstname, lastname, city, country, tags, tagline, price, portrait } = element;
-    const thumbnail = photographeThumbPath.concat(portrait);
-    const tagList = tags.join('</button></li><li class="tag"><button class="btn-tag">#');
-
-    const uiCard = `<article class="card"><img class="card__img" src="${thumbnail}" height="200" width="200"><h2>${firstname} ${lastname}</h2><h3>${city}, ${country}</h3><p>${tagline}</p><p>${price}€/jour</p><ul class="tag-list"><li class="tag"><button class="btn-tag">#${tagList}</button></li></ul></article>`;
-    uiPhotographersList.insertAdjacentHTML("beforeend", uiCard);
-  });
-};
-
-// test only
-// add photographers to the table
-const addPhotographersTable = elements => {
-  elements.forEach(element => {
-    const { id, firstname, lastname, city, country, tags, tagline, price, portrait, medias } = element;
-    const tagString = tags.join(", ");
-    const titlesArray = [];
-    medias.forEach(element => {
-      titlesArray.push(element.title);
-    });
-    const titles = titlesArray.join(", ");
-    const row = `<tr><td>${id}</td><td>${firstname}</td><td>${lastname}</td><td>${city}</td><td>${country}</td><td>${tagString}</td><td>${tagline}</td><td>${price}</td><td>${portrait}</td><td>${titles}</td></tr>`;
-    photographersTable.insertAdjacentHTML("beforeend", row);
-  });
-};
-
-// test only
-// add medias to the table
-const addMediasTable = elements => {
-  elements.forEach(element => {
-    const { id, photographerId, title, image, video, tags, likes, date, price, photographer } = element;
-    const tagString = tags.join(", ");
-    const row = `<tr><td>${id}</td><td>${photographerId}</td><td>${photographer[0]} ${photographer[1]}</td><td>${title}</td><td>${image}</td><td>${video}</td><td>${tagString}</td><td>${likes}</td><td>${date}</td><td>${price}</td></tr>`;
-    mediasTable.insertAdjacentHTML("beforeend", row);
-  });
-};
---
+++ assets/js/builder.js
@@ -0,0 +1,79 @@
+import { getPhotographerById, getMediasByPhotographer } from "./helpers.js";
+
+// photographers factory
+const photographersFactory = (id, firstname, lastname, city, country, tags, tagline, price, portrait) => {
+  const medias = [];
+  const addMedia = element => {
+    medias.push(element);
+  };
+  return { id, firstname, lastname, city, country, tags, tagline, price, portrait, medias, addMedia };
+};
+
+// medias factory
+const mediasFactory = (id, photographerId, title, image, video, tags, likes, date, price) => {
+  const photographer = [];
+  const addPhotographer = element => {
+    photographer.push(element);
+  };
+  return { id, photographerId, title, image, video, tags, likes, date, price, photographer, addPhotographer };
+};
+
+// function initialisation photographers
+const buildPhotographers = elements => {
+  const photographersInitArray = [];
+  elements.forEach(element => {
+    const { id, name, city, country, tags, tagline, price, portrait } = element;
+    const fullname = name.split(" ");
+    const firstname = fullname[0];
+    const lastname = fullname[1];
+    const photographer = photographersFactory(id, firstname, lastname, city, country, tags, tagline, price, portrait);
+    photographersInitArray.push(photographer);
+  });
+  return photographersInitArray;
+};
+
+// function initialisation media
+const buildMedias = elements => {
+  const mediasInitArray = [];
+  elements.forEach(element => {
+    const { id, photographerId, title, image, video, tags, likes, date, price } = element;
+    const media = mediasFactory(id, photographerId, title, image, video, tags, likes, date, price);
+    mediasInitArray.push(media);
+  });
+  return mediasInitArray;
+};
+
+// function expand photographers with medias
+const expandPhotographers = (photographers, medias) => {
+  photographers.forEach(photographer => {
+    const media = getMediasByPhotographer(medias, photographer.id);
+    media.forEach(element => {
+      photographer.addMedia(element);
+    });
+  });
+  return photographers;
+};
+
+// function expand medias with photographer
+const expandMedias = (photographers, medias) => {
+  medias.forEach(media => {
+    const photographer = getPhotographerById(photographers, media.photographerId);
+    media.addPhotographer(photographer.firstname);
+    media.addPhotographer(photographer.lastname);
+  });
+  return medias;
+};
+
+// function create photographers
+const buildData = elements => {
+  // init
+  const photographersInit = buildPhotographers(elements.photographers);
+  const mediasInit = buildMedias(elements.media);
+
+  // add medias photographers and photographers to media
+  const photographers = expandPhotographers(photographersInit, mediasInit);
+  const medias = expandMedias(photographersInit, mediasInit);
+  return { photographers, medias };
+};
+
+export default buildData;
