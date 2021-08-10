e90bc7671a8785228e70f998974f4bb2e9cfacfc
Joska Potin <jpotin@gmail.com>
2021-08-10 22:28:25 +0200

    fix init

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,23 +1,31 @@
-import initHomepage from "./homepage.js";
 import { removeFromDom } from "./helpers.js";
+import initHomepage from "./homepage.js";
 
+// DOM elements
 const homeLink = document.getElementById("link-to-homepage");
 
+// Remove all the elements
 const resetApp = () => {
   const elements = [document.getElementById("photographers-list"), document.getElementById("tag-nav")];
   return removeFromDom(elements);
 };
 
+// Home link
 homeLink.addEventListener("click", function (event) {
   resetApp();
   initHomepage();
 });
 
-const initApp = url => {
-  const isRoot = /^(\/|\/index\.html)$/i.test(location.pathname);
-  if (isRoot) {
+// Init
+const initApp = () => {
+  // const currentUrl = location.href;
+  // const currentPath = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
+  // const isRoot = /^(\/|\/index\.html)$/i.test(location.pathname);
+  console.log(location.pathname);
+  if (location.pathname === "/" || location.pathname === "/index.html") {
     initHomepage();
   }
+  // initHomepage();
 };
 
 initApp();
