0e8ba4fe9b9b23fa60cf462f0741cf1f0eca4928
Joska Potin <jpotin@gmail.com>
2021-08-12 19:31:42 +0200

    path fix again

--- src/assets/js/options.js
+++ src/assets/js/options.js
@@ -1,6 +1,6 @@
 // Env
-const apiUrl = "data/fisheyedata.json"
-const mediasPath = "medias"
+const apiUrl = "./data/fisheyedata.json"
+const mediasPath = "./medias"
 const photographeThumbPath = mediasPath.concat("/photographers-id-photos")
 // DOM elements
 const homeLink = document.getElementById("link-to-homepage")
