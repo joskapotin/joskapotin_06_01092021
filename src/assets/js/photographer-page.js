e555ea7648dcf2d9e49894c61403a416ada70a96
Joska Potin <jpotin@gmail.com>
2021-08-16 14:12:00 +0200

    photographer resume layout

--- src/assets/js/photographer-page.js
+++ src/assets/js/photographer-page.js
@@ -4,7 +4,9 @@ import { uiInsertGallery, requestMediasByPhotographer, createTagNav, initTagNav,
 const uiCreateHeader = ({ id, name, city, country, tags, tagline, portrait, price }) => {
   const thumbnail = `${photographeThumbPath}/${portrait}`
   const tagNav = createTagNav(tags)
-  const header = `<section class="photographer-section" data-id="${id}"><article class="photographer__content"><h1 class="photographer__name">${name}</h1><h2 class="photographer__location">${city}, ${country}</h2><p class="photographer__tagline">${tagline}</p>${tagNav}</article><button class="btn photographer__btn-contact">Contactez-moi</button><aside class="photographer__aside"><span class="photographer__likes">297 081</span><span class="photographer__pricing">${price}€/jour</span></aside><picture class="photographer__picture"><img class="photographer__img" src="${thumbnail}"></picture></section>`
+  const header = `<section class="photographer-section" data-id="${id}" data-reset><article class="photographer__resume">
+    <h1 class="photographer__name">${name}</h1><h2 class="photographer__location">${city}, ${country}</h2><p class="photographer__tagline">${tagline}</p>${tagNav}
+  </article><button class="btn photographer__btn-contact">Contactez-moi</button><picture class="photographer__picture"><img class="photographer__img" src="${thumbnail}"></picture><aside class="photographer__aside"><span class="photographer__likes">297 081</span><span class="photographer__pricing">${price}€/jour</span></aside></section>`
   uiHeader.insertAdjacentHTML("beforeend", header)
 }
 
@@ -12,7 +14,7 @@ const initPhotographerPage = async (apiUrl, photographer) => {
   resetPage()
   uiCreateHeader(photographer)
 
-  const markup = `<nav class="sort-nav"><span class="sort-nav__label">Trier par</span><ul class="sort-nav__list" tabindex="0"><li class="sort-nav__item" data-sorter="Likes">Popularité</li><li class="sort-nav__item" data-sorter="Date">Date</li><li class="sort-nav__item" data-sorter="Title">Titre</li></ul></nav><section id="media-gallery" class="media-gallery"></section>`
+  const markup = `<nav class="sort-nav" data-reset><span class="sort-nav__label">Trier par</span><ul class="sort-nav__list" tabindex="0"><li class="sort-nav__item" data-sorter="Likes">Popularité</li><li class="sort-nav__item" data-sorter="Date">Date</li><li class="sort-nav__item" data-sorter="Title">Titre</li></ul></nav><section id="media-gallery" class="media-gallery" data-reset></section>`
   uiMain.insertAdjacentHTML("beforeend", markup)
 
   const medias = await requestMediasByPhotographer(apiUrl, photographer.id)
