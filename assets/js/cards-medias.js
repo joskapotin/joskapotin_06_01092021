5c6843a7d8081bbfe335757118b9e2eb9df02090
joskapotin <joskapotin@users.noreply.github.com>
2021-08-20 12:12:01 +0000

    video overlay 04970ebae8c609672690b40c2be8c1db53cbc550

--- assets/js/cards-medias.js
+++ assets/js/cards-medias.js
@@ -8,14 +8,14 @@ const createMediasCards = (medias, photographerId) => {
     const photographerMediaPath = `${mediasPath}/photographer-id-${photographerId}`
     const alt = image?.slice(0, -4).replaceAll("_", " ") ?? video?.slice(0, -4).replaceAll("_", " ")
 
-    let thumbnail = `<img class="media media-img" alt="${alt}" data-src="${photographerMediaPath}/${image}" src="${photographerMediaPath}/${image}">`
+    let thumbnail = `<figure class="media__figure"><img class="media media-img" alt="${alt}" data-src="${photographerMediaPath}/${image}" src="${photographerMediaPath}/${image}"></figure>`
 
     if (video) {
       const ext = video.substr(video.lastIndexOf(".") + 1)
-      thumbnail = `<span class="visually-hidden">${alt}</span><video tabindex="-1" class="media media-video" data-src="${photographerMediaPath}/${image}"><source src="${photographerMediaPath}/${video}" type="video/${ext}"></video>`
+      thumbnail = `<figure class="media__figure media__figure-video"><video tabindex="-1" class="media media-video" data-src="${photographerMediaPath}/${image}"><source src="${photographerMediaPath}/${video}" type="video/${ext}"></video><figcaption class="visually-hidden">${alt}</figcaption></figure>`
     }
 
-    const uiMedia = `<article class="media__content" data-id="${id}" data-tag-filtrable><a href="media/${id}" class="media__link"><figure class="media__figure">${thumbnail}</figure></a><footer class="media__footer"><h3 class="media__title">${title}</h3><span class="media__date">${date}</span><span class="media__price">${price}€</span><span class="media__total-likes">${likes}<button class="btn-like">Like</button></span></footer></article>`
+    const uiMedia = `<article class="media__content" data-id="${id}" data-tag-filtrable><a href="media/${id}" class="media__link">${thumbnail}</a><footer class="media__footer"><h3 class="media__title">${title}</h3><span class="media__date">${date}</span><span class="media__price">${price}€</span><span class="media__total-likes">${likes}<button class="btn-like">Like</button></span></footer></article>`
     mediasCards.push(uiMedia)
   })
 
