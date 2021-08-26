ed14d9cff32cf1736e5297d422c2101b18762517
joskapotin <joskapotin@users.noreply.github.com>
2021-08-26 19:12:28 +0000

    end of js comments 3132490524cd0c8e917172b4f12688022b58a799

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,5 @@
 import { config } from "./config.js"
 import initHomePage from "./page-home.js"
-
-// Init
-// const initApp = apiUrl => {
-//   uiHomeLink.addEventListener("click", event => {
-//     initHomePage(apiUrl)
-//   })
-
-//   initHomePage(apiUrl)
-// }
-// initApp(apiUrl)
-
-// IIFE : Immediatly invoued functin expression
 ;(() => {
   config.uiHomeLink.addEventListener("click", event => {
     event.preventDefault()
