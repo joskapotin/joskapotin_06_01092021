0f74651cd3f8fa6c75428fbe1c7829e8424d9364
Joska Potin <jpotin@gmail.com>
2021-10-28 16:25:56 +0200

    fix

--- public/assets/js/utils/factory.js
+++ public/assets/js/utils/factory.js
@@ -2,9 +2,7 @@ import Image from "../models/image.js"
 import Video from "../models/Video.js"
 
 const mediaFactory = media => {
-  const element =  media.image ? new Image(media) : new Video(media)
-
-  if
+  return media.image ? new Image(media) : new Video(media)
 }
 
 export default mediaFactory
