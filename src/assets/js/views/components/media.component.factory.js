cb876c4e9f0121a874df9dab079a3934ad036a42
Joska Potin <jpotin@gmail.com>
2021-10-29 10:27:53 +0200

    refactor media factory

--- src/assets/js/views/components/media.component.factory.js
+++ src/assets/js/views/components/media.component.factory.js
@@ -9,39 +9,49 @@ import config from "../../config/config.js"
  * @returns {HTMLElement} uiFigure
  */
 const mediaFactory = ({ photographerId, media, type, alt }) => {
+  if (type === "image") {
+    return createImage({ photographerId, media, alt })
+  } else if (type === "video") {
+    return createVideo({ photographerId, media, alt })
+  }
+}
+
+const createImage = ({ photographerId, media, alt }) => {
   const photographerMediaPath = `${config.mediasPath}/photographer-id-${photographerId}`
   const altText = alt
 
   const uiFigure = document.createElement("figure")
   uiFigure.className = "media__figure"
+  uiFigure.classList.add("media__figure--img")
+  const uiThumbnail = document.createElement("img")
+  uiThumbnail.classList.add("media", "media--img")
+  uiThumbnail.alt = altText
+  uiThumbnail.src = `${photographerMediaPath}/${media}`
+  uiThumbnail.dataset.src = `${photographerMediaPath}/${media}`
+  uiFigure.appendChild(uiThumbnail)
 
-  if (type === "image") {
-    uiFigure.classList.add("media__figure--img")
-    const uiThumbnail = document.createElement("img")
-    uiThumbnail.classList.add("media", "media--img")
-    uiThumbnail.alt = altText
-    uiThumbnail.src = `${photographerMediaPath}/${media}`
-    uiThumbnail.dataset.src = `${photographerMediaPath}/${media}`
+  return uiFigure
+}
 
-    uiFigure.appendChild(uiThumbnail)
-  } else if (type === "video") {
-    uiFigure.classList.add("media__figure--video")
-    const uiThumbnail = document.createElement("video")
-    uiThumbnail.classList.add("media", "media--video")
+const createVideo = ({ photographerId, media, type, alt }) => {
+  const photographerMediaPath = `${config.mediasPath}/photographer-id-${photographerId}`
+  const altText = alt
 
-    const ext = media.substr(media.lastIndexOf(".") + 1)
-    const source = document.createElement("source")
-    source.src = `${photographerMediaPath}/${media}`
-    source.type = `video/${ext}`
+  const uiFigure = document.createElement("figure")
+  uiFigure.className = "media__figure"
+  uiFigure.classList.add("media__figure--video")
+  const uiThumbnail = document.createElement("video")
+  uiThumbnail.classList.add("media", "media--video")
+  const ext = media.substr(media.lastIndexOf(".") + 1)
+  const source = document.createElement("source")
+  source.src = `${photographerMediaPath}/${media}`
+  source.type = `video/${ext}`
+  uiThumbnail.append(source)
+  const uiFigcaption = document.createElement("figcaption")
+  uiFigcaption.className = "visually-hidden"
+  uiFigcaption.textContent = altText
 
-    uiThumbnail.append(source)
-
-    const uiFigcaption = document.createElement("figcaption")
-    uiFigcaption.className = "visually-hidden"
-    uiFigcaption.textContent = altText
-
-    uiFigure.append(uiThumbnail, uiFigcaption)
-  }
+  uiFigure.append(uiThumbnail, uiFigcaption)
 
   return uiFigure
 }
